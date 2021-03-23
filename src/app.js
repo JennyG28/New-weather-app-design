d98f8078be0fcb9b666b88a1415db389d1143ef9
JennyG28 <78384229+JennyG28@users.noreply.github.com>
2021-03-23 17:04:13 -0500

    Added background and images

--- src/app.js
+++ src/app.js
@@ -66,7 +66,6 @@
         document.querySelector("#temperature-number").innerHTML = Math.round(
           response.data.main.temp
         );
-        let descriptionElement = document.querySelector("#weather-description");
         document.querySelector("#weather-description").innerHTML =
           response.data.weather[0].description;
 
@@ -86,7 +85,52 @@
           "src",
           `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
         );
+
+        let pictureElement = document.querySelector("#picture");
+        let background = response.data.weather[0].icon;
+        if (background === "01d"){
+        backgroundImage.style = "background-image: linear-gradient(to top, #96fbc4 0%, #f9f586 100%)";
+        pictureElement.setAttribute(
+          "src",
+          `src/img-1.png`
+        );
+        } else if (background === "02d") {
+          backgroundImage.style = "background-image: linear-gradient(to right, #ff758c 0%, #ff7eb3 100%)";
+           pictureElement.setAttribute(
+          "src",
+          `src/img-2.png`
+        );
+        } else if (background === "03d" || background === "04d") {
+          backgroundImage.style = "background-image: linear-gradient(to top, #c4c5c7 0%, #dcdddf 52%, #ebebeb 100%);";
+           pictureElement.setAttribute(
+          "src",
+          `src/img-3.png`
+        );
+        } else if (background === "09d" || background === "10d" || background === "11d") {
+          backgroundImage.style = "background-image: linear-gradient(-225deg, #473B7B 0%, #3584A7 51%, #30D2BE 100%);";
+           pictureElement.setAttribute(
+          "src",
+          `src/img-4.png`
+        );
+        } else if (background === "13d") {
+          backgroundImage.style = "background-image: linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%);";
+           pictureElement.setAttribute(
+          "src",
+          `src/img-5.png`
+        );
+         } else if (background === "50d") {
+          backgroundImage.style = "background-image: linear-gradient(to top, #ebc0fd 0%, #d9ded8 100%);";
+           pictureElement.setAttribute(
+          "src",
+          `src/img-6.png`
+        );
+        } 
+      
+       
+        
       }
+      
+
       function searchCity(city) {
         let apiKey = "c330d6d567e845b62d32598b378046e4";
         //The default temperature unit is °F
@@ -126,7 +170,7 @@
         temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
         let fahrenheitLink = document.querySelector("#fahrenheit-link");
         fahrenheitLink.style = "color: #000000";
-        celsiusLink.style = "color: #C0C0C0";
+        celsiusLink.style = "color: #808080";
       }
 
       function displayCelsiusTemperature(event) {
@@ -135,7 +179,7 @@
         temperatureElement.innerHTML = Math.round(celsiusTemperature);
         let celsiusLink = document.querySelector("#celsius-link");
         celsiusLink.style = "color: #000000";
-        fahrenheitLink.style = "color: #C0C0C0";
+        fahrenheitLink.style = "color: #808080";
       }
 
       //Global variable. It can be accessed from any function. It will have no value "null" when page is load.
@@ -143,7 +187,7 @@
 
       //When this is click the funtion displayCelsiusTemperature is called
       let fahrenheitLink = document.querySelector("#fahrenheit-link");
-      fahrenheitLink.style = "color: #C0C0C0";
+      fahrenheitLink.style = "color: #808080";
       fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
       let celsiusLink = document.querySelector("#celsius-link");
